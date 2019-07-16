10e75e62fa7cc615467cc7a6814815dd5a17f4a7
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-07-16 16:36:12 -0700

    commit

--- controllers/food2forkController.js
+++ controllers/food2forkController.js
@@ -7,11 +7,13 @@ const apiRecipeUrl = process.env.DB_F2FRECIPEURL;
 module.exports = {
   searchFood2Fork: function(req, res) {
     console.log(apiUrl + apiKey, { params: req.query });
-    console.log('https://www.food2fork.com/api/search?key=64163ecfe7d2c2321832cea3563bf507&rId=35382');
+    console.log('https://www.food2fork.com/api/search?key=d22268506cdee3e2a2d2a26dba0a685e&rId=35382');
 
     axios
       .get(apiUrl + apiKey, { params: req.query })
-      .then(results => results.data.items)
+      .then(results =>  object =Json.stringify(results.recipes),
+        
+        console.log("myresult",object))
       .then(recipes => res.json(recipes))
       .catch(err => res.status(422).json(err));
   }
