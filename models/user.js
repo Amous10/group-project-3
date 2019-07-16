970b333e47ab61365ca207f4dbc6edef6ed02966
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-07-15 17:57:05 -0700

    another commit

--- models/user.js
+++ models/user.js
@@ -6,8 +6,8 @@ mongoose.promise = Promise;
 // Define userSchema
 const userSchema = new Schema({
   username: { type: String, unique: false, required: false },
-  password: { type: String, unique: false, required: false }
-  /* book: {
+  password: { type: String, unique: false, required: false },
+  book: {
     type: Schema.Types.ObjectId,
     ref: 'Book'
   }
