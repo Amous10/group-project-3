7ef019fd73bc4f505948d4d1afdd65cb9ebbdc3d
Kevin Flanagan <kflan3000@gmail.com>
2019-07-17 09:24:27 -0700

    pre merge

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Route, Link } from 'react-router-dom';
-import logo from '../logo.svg';
+// import logo from '../img/logo.svg';
+import logo from '../img/pot.svg';
+
 import '../App.css';
 import axios from 'axios';
 
